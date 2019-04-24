755c6799a888b50eba64aed3eb2855e8efa57579
kalyancc <kalyancc@gmail.com>
2019-04-24 23:23:33 +0100

    added chrome

--- src/test/java/sample/TasksTests.java
+++ src/test/java/sample/TasksTests.java
@@ -9,7 +9,7 @@ public class TasksTests {
 	public void createCustomerTest() {
 		System.out.println("********************************************************************************************8");
 		Common lib = new Common();
-		WebDriver dr = lib.OpenBrowser("FireFox");
+		WebDriver dr = lib.OpenBrowser("Chrome");
 		lib.Login(dr);
 		lib.CreateNewCustomer(dr);
 		///test
